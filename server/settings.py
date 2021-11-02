a8abf1574c9999049a1b1c8bb2591c7f8fdc7977
lee sang hun <oper6909@gmail.com>
2021-11-03 03:01:35 +0900

    feat: make custom user

--- server/settings.py
+++ server/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs',
 ]
+
+AUTH_USER_MODEL = 'blogs.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
